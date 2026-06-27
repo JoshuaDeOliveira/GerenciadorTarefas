e0f82fa4800f10275c5587ad9c886491d8bd8e91
Joshua Erbe <joshuadultra@gmail.com>
2026-06-27 00:29:02 -0300

    squema.sql base feito

---
+++ database/squema.sql
@@ -0,0 +1,80 @@
+DROP DATABASE IF EXISTS tarefas_dados;
+CREATE DATABASE tarefas_dados;
+USE tarefas_dados;
+
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+	id_user BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name_user VARCHAR(50) NOT NULL,
+    password_user VARCHAR(30) NOT NULL,
+    email_user VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE tasks (
+	id_task BIGINT AUTO_INCREMENT PRIMARY KEY,
+    who_task BIGINT,
+    task_name VARCHAR(100) NOT NULL,
+    task_date DATETIME NOT NULL,
+    CONSTRAINT fk_who_task FOREIGN KEY (who_task) 
+		REFERENCES users(id_user)
+);
+
+CREATE TABLE log_operation (
+	id_log BIGINT AUTO_INCREMENT PRIMARY KEY,
+    type_operation ENUM("INSERT", "DELETE", "UPDATE") NOT NULL,
+    date_operation TIMESTAMP NOT NULL
+);
+
+DELIMITER $$
+
+CREATE TRIGGER tr_log_insert_users AFTER INSERT
+ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_operation (type_operation, date_operation)
+    VALUES ("INSERT", NOW());
+END $$
+
+CREATE TRIGGER tr_log_delete_users AFTER DELETE
+ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_operation (type_operation, date_operation)
+    VALUES ("DELETE", NOW());
+END $$
+
+CREATE TRIGGER tr_log_update_users AFTER UPDATE
+ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_operation (type_operation, date_operation)
+    VALUES ("UPDATE", NOW());
+END $$
+
+CREATE TRIGGER tr_log_insert_tasks AFTER INSERT
+ON tasks
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_operation (type_operation, date_operation)
+    VALUES ("INSERT", NOW());
+END $$
+
+CREATE TRIGGER tr_log_delete_tasks AFTER DELETE
+ON tasks
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_operation (type_operation, date_operation)
+    VALUES ("DELETE", NOW());
+END $$
+
+CREATE TRIGGER tr_log_update_tasks AFTER UPDATE
+ON tasks
+FOR EACH ROW
+BEGIN
+	INSERT INTO log_operation (type_operation, date_operation)
+    VALUES ("UPDATE", NOW());
+END $$
+
+DELIMITER ;
