ba5a9fac7cd36e965e05131522456a10974e792b
Joshua Erbe <joshuadultra@gmail.com>
2026-07-04 00:08:22 -0300

    Rotas de usuarios + rotas de tarefas

--- src/Node.js/routes/tasks.js
+++ src/Node.js/routes/tasks.js
@@ -2,22 +2,114 @@ import express from 'express';
 import poolconnect from '../connect.js'
 
 const tasks_routes = express.Router()
+var results = {}
 
 tasks_routes.get('/tasks', async function (req, res) {
     try {
-        const [results] = await poolconnect.execute(
+        [results] = await poolconnect.execute(
             `SELECT * FROM tasks`
         )
 
         res.status(200).json(results)
     } catch (error){
-        const results = {
+        results = {
             message: `Foi encontrado esse erro ${error}`,
             code: 500
         }
 
         res.status(results.code).send(results)
     }
+})
+
+tasks_routes.post('/SendTask', async function (req, res) {
+    try {
+        const emailuser = req.query.email_user
+        const taskname = req.body.task_name
+        const taskdate = req.body.task_date
+
+        const [user_id] = await poolconnect.execute(
+            `SELECT id_user FROM users WHERE email_user = ?`, [emailuser]
+        )
+        const who_id = user_id[0].id_user
+
+        const [response] = await poolconnect.execute(
+            `INSERT INTO tasks(who_task, task_name, task_date)
+            VALUES (?, ?, ?)`, [who_id, taskname, taskdate]
+        )
+
+        if (response.affectedRows > 0) {
+            results = {
+                message: 'Nova tarefa adicionada',
+                code: 200
+            }
+        } else {
+            results = {
+                message: 'Tarefa não foi adicionada!',
+                code: 500
+            }
+        }
+    } catch (error) {
+        results = {
+            message: `Esse foi o erro encontrado: ${error}`,
+            code: 500
+        }
+    }
+
+    res.status(results.code).send(results)
+})
+
+tasks_routes.put('/UpdateTask', async function (req, res) {
+    try {
+        const idtask = req.query.id_task
+        const iduser = req.query.id_user
+
+        const [ListTasks] = await poolconnect.execute(
+            'SELECT * FROM tasks WHERE who_task = ?', [iduser]
+        )
+
+        console.log(ListTasks)
+
+        results = {
+            message: 'Teste efetuado com sucesso',
+            code: 200
+        }
+    } catch (error) {
+        results = {
+            message: `Esse foi o erro encontrado: ${error}`,
+            code: 500
+        }
+    }
+
+    res.status(results.code).json(results)
+})
+
+tasks_routes.delete('/DeleteTask', async function (req, res) {
+    try {
+        const idtasks = req.query.id_task 
+
+        const [response] = await poolconnect.execute(
+            'DELETE FROM tasks WHERE id_task = ?', [idtasks]
+        ) 
+
+        if (response.affectedRows > 0) {
+            results = {
+                message: 'Tarefa deletada!',
+                code: 200
+            }
+        } else {
+            results = {
+                message: 'Não foi possivel deletar a tarefa!',
+                code: 500
+            }
+        }
+    } catch (error) {
+        results = {
+            message: `Esse foi o erro encontrado: ${error}`,
+            code: 500
+        }
+    }
+
+    res.status(results.code).json(results)
 })
 
 export default tasks_routes
--- src/Node.js/routes/users.js
+++ src/Node.js/routes/users.js
@@ -2,16 +2,17 @@ import express from 'express';
 import poolconnect from '../connect.js'
 
 const user_routes = express.Router()
+var results = {}
 
-user_routes.get('/users', async function (req, res) {
+user_routes.get('/Users', async function (req, res) {
     try {
-        const [results] = await poolconnect.execute(
+        [results] = await poolconnect.execute(
             'SELECT * FROM users'
         )
 
         res.status(200).json(results)
     } catch (error) {
-        const results = {
+        results = {
             message: `Foi encontrado esse erro ${error}`,
             code: 500
         }
@@ -20,9 +21,8 @@ user_routes.get('/users', async function (req, res) {
     }
 })
 
-user_routes.post('/registerusers', async function (req, res) {
+user_routes.post('/RegisterUser', async function (req, res) {
     try {
-
         const nameuser = req.body.name
         const passworduser = req.body.password
         const emailuser = req.body.email
@@ -33,20 +33,78 @@ user_routes.post('/registerusers', async function (req, res) {
             [nameuser, passworduser, emailuser]
         )
 
-        const results = {
-            message: "Usuario cadastrado com sucesso",
-            code: 200
+        if (response.affectedRows > 0) {
+            results = {
+                message: "Usuario cadastrado com sucesso",
+                code: 200
+            }
         }
-
-        res.status(results.code).json(results)
     } catch (error) {
-        const results = {
+         results = {
             message: `Foi encontrado esse erro ${error}`,
             code: 500
-        }
-
-        res.status(results.code).send(results)
+        }  
     }
+
+    res.status(results.code).json(results)
+})
+
+user_routes.put('/UpdateUser', async function (req, res) {
+    try {
+        const attname = req.body.newname
+        const attpassword = req.body.newpassword
+        const attemail = req.body.newemail
+        const id_user = req.body.iduser
+
+        const [response] = await poolconnect.execute(
+            `UPDATE users SET name_user = ?, password_hash = ?, email_user = ?
+            WHERE id_user = ?`, [attname, attpassword, attemail, id_user]
+            /*Lembrete para mim mesmo do futuro, lembrar de colocar uma forma de o usuario escolher o que atualizar*/
+        )
+        
+        if (response.affectedRows > 0) {
+            results = {
+                message: 'Usuario atualizado com sucesso',
+                code: 200
+            }
+        }
+    } catch (error) {
+        results = {
+            message: `foi encontrado esse erro na tentativa de atualizar o usuario ${error}`,
+            code: 500
+        }
+    }
+
+    res.status(results.code).json(results)
+})
+
+user_routes.delete('/DeleteUser', async function (req, res) {
+    try {
+        const iduser = req.query.id_user
+
+        const [response] = await poolconnect.execute(
+            'DELETE FROM users WHERE id_user = ?', [iduser]
+        )
+
+        if (response.affectedRows > 0) {
+            results = {
+                message: `Usuario foi deletado com sucesso!`,
+                code: 200
+            }
+        } else {
+            results = {
+                message: `Nao foi possivel deletar usuario!`,
+                code: 500
+            }
+        }
+    } catch (error) {
+        results = {
+            message: `Esse foi erro encontrado ${error}`,
+            code: 500
+        }
+    }
+
+    res.status(results.code).json(results)
 })
 
 export default user_routes
